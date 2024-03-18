8c2a254958b8697a7881c8d4c84856a62b8a0ff7
sARTHAK-618 <sarthakshelar618@gmail.com>
2024-03-18 20:45:25 +0530

    added_two_of_the_graphs_in_gui_code

--- trial_1/matplotlib_learn/spin_graph_info.py
+++ trial_1/matplotlib_learn/spin_graph_info.py
@@ -9,4 +9,17 @@ ax1.plot(xs, ys)
 ax1.set_title("Humidity")
 ax1.set_xlabel("Humidity(in percetange)")
 ax1.set_ylabel("Time(in Second)")
-plt.show()
+#plt.show()
+
+
+
+
+fig1, ax1 = plt.subplots()
+ys = [20, 42, 39, 45, 36, 49, 45]
+
+xs = [1, 2, 3, 4, 5, 6, 7]
+ax1.plot(xs, ys)
+ax1.set_title("Temperature")
+ax1.set_xlabel("Temerature(in celcius)")
+ax1.set_ylabel("Time(in Second)")
+#plt.show()
